2f772b1d22a5709bc23be98e54d950b4d23ccddf
OmGor2022 <Omgor2022@gmail.com>
2022-03-05 00:41:51 +0530

    theme.json, Foundation Setup

--- themes/herbalbeauty/functions.php
+++ themes/herbalbeauty/functions.php
@@ -89,6 +89,8 @@ function herbalbeauty_setup() {
 			'flex-height' => true,
 		)
 	);
+
+	add_theme_support( 'wp-block-styles' );
 }
 add_action( 'after_setup_theme', 'herbalbeauty_setup' );
 
@@ -128,9 +130,35 @@ add_action( 'widgets_init', 'herbalbeauty_widgets_init' );
  * Enqueue scripts and styles.
  */
 function herbalbeauty_scripts() {
-	wp_enqueue_style( 'herbalbeauty-style', get_stylesheet_uri(), array(), HERBALBEAUTY_VERSION );
+	wp_enqueue_style(
+	 'herbalbeauty-style',
+	 get_stylesheet_uri(),
+	 array(),
+	 HERBALBEAUTY_VERSION
+	);
 
-	
+	wp_enqueue_style(
+		'foundation-style',
+		get_template_directory_uri() . '/assets/css/vendor/foundation.min.css'
+		array(),
+		'6.7.4'
+	   );
+
+	wp_enqueue_script(
+		'what-input-script', 
+		get_template_directory_uri() . '/assets/js/vendor/what-input.js',
+		array( 'jquery' ), 
+		'5.2.10'
+		true
+	);
+
+	wp_enqueue_script(
+		'foundation-script', 
+		get_template_directory_uri() . '/assets/js/vendor/foundation.min.js',
+		array( 'jquery', 'what-input-script' ), 
+		'6.7.4'
+		true
+	);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
