b78001f7e0385597ec66a69ef0a232588d593148
OmGor2022 <Omgor2022@gmail.com>
2022-03-08 13:40:33 +0530

    Changes in Theme

--- themes/herbalbeauty/inc/woocommerce.php
+++ themes/herbalbeauty/inc/woocommerce.php
@@ -24,5 +24,27 @@
  /**
   * Re-add product title
   */
+  add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 15 );
+
+  /**
+   * Move the excerpt
+   */
   remove_action ( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
   add_action( 'woocommerce_single_product_summery', 'woocommerce_template_single_excerpt', 4 );
+
+  /**
+   * Add sample text above add to card
+   */
+  function harbelbeauty_add_sample_text() {
+      echo '<p style="color:#ff5252;"><b>You Will Love Our Products</b></p>';
+  }
+  add_action( 'woocommerce_before_add_to_cart_form', 'add_sample_text', 10 );
+
+  /**
+   * Remove tab data
+   */
+  remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tab', 10 );
+  function harbelbeauty_add_content() {
+      echo '<div class="content">' . get_the_content() . '</div>';
+  }
+  add_action( 'woocommerce_after_single_product_summary', 'herbalbeauty_add_content', 10 );
