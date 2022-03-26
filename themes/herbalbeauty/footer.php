e225f3281df5082a3cdd136355a90d181354cff4
OmGor2022 <Omgor2022@gmail.com>
2022-03-26 15:58:42 +0530

    Remove Unnecessary File

--- themes/herbalbeauty/footer.php
+++ themes/herbalbeauty/footer.php
@@ -12,6 +12,40 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
+		<?php
+		$recipe_args = array(
+			'post_types'     => array( 'herbalbeauty_recipe' ),
+			'posts_status'   => 'publish',
+			'posts_per_page' => 3,
+			'orderby'        => 'rand',
+			'post_not_in'    => array( get_the_ID() )
+		);
+
+		$recipe_query = new WP_Query( $recipe_args );
+
+		if( $recipe_query->have_posts() ) {
+			?>
+			<div class="grid-container">
+				<div class="grid-x grid-margin-x grid-margin-y">
+					<?php
+					while ( $recipe_query->have_posts() ) {
+						$recipe_query->the_posts();
+						?>
+						<div class="cell small-12 medium-4">
+							<?php
+							the_post_thumbnail();
+							the_title( '<h3>', '</h3>' );
+							the_excerpt();
+							?>
+						</div>
+						<?php
+					}
+					wp_reset_postdata();
+					?>
+				</div>
+			</div>
+			<?php } ?>
+		
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'herbalbeauty' ) ); ?>">
 				<?php
