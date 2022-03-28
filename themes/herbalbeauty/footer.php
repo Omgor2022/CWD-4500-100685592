637d86c04be5c8d251437ce294406885f39d5427
OmGor2022 <Omgor2022@gmail.com>
2022-03-28 12:02:43 +0530

    Add Footer Widgets Area

--- themes/herbalbeauty/footer.php
+++ themes/herbalbeauty/footer.php
@@ -11,56 +11,44 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<?php
-		$recipe_args = array(
-			'post_types'     => array( 'herbalbeauty_recipe' ),
-			'posts_status'   => 'publish',
-			'posts_per_page' => 3,
-			'orderby'        => 'rand',
-			'post_not_in'    => array( get_the_ID() )
-		);
+<footer>
+	<div id="footer-1">
 
-		if( $recipe_query=>have_posts() ) {
-			?>
-			<div class="grid-container">
-				<div class="grid-x grid-margin-x grid-margin-y">
-					<?php
-					while ( $recipe_query=>have_posts() ) {
-						$recipe_query=>the_posts();
-						?>
-						<div class="cell small-12 medium-4">
-							<?php
-							the_post_thumbnail();
-							the_title( '<h3>', '</h3>' );
-							the_excerpt();
-							?>
-						</div>
-						<?php
-					}
-					wp_reset_postdata();
-					?>
-				</div>
-			</div>
-			<?php } ?>
-		
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'herbalbeauty' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'herbalbeauty' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'herbalbeauty' ), 'herbalbeauty', '<a href="http://underscores.me/">Om Gor</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	<?php
+	if(is_active_sidebar('footer-1')){
+	dynamic_sidebar('footer-1');
+	}
+	?>
+	</div>
 
-<?php wp_footer(); ?>
+	<div id="footer-2">
 
-</body>
-</html>
+	<?php
+	if(is_active_sidebar('footer-2')){
+	dynamic_sidebar('footer-2');
+	}
+	?>
+	</div>
+
+	<div id="footer-3">
+
+	<?php
+	if(is_active_sidebar('footer-3')){
+	dynamic_sidebar('footer-3');
+	}
+	?>
+
+	</div>
+
+	<div id="footer-4">
+
+	<?php
+	if(is_active_sidebar('footer-4')){
+	dynamic_sidebar('footer-4');
+	}
+	?>
+
+
+	</div>
+         
+</footer>
--- themes/herbalbeauty/template-parts/content.php
+++ themes/herbalbeauty/template-parts/content.php
@@ -12,13 +12,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
+		if ( ! is_singular( 'product') ) {
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
-			if ( get_the_title() ) {
-			?>
-			<h1><?php echo_get_the_title(); ?></h1>
-			<?php
-			}
 		 else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		 endif;
